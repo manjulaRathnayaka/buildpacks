71bdc1d3eb021cc4605365c5ae96f6bb3a70290f
Manjula Rathnayaka <manjula.cse@gmail.com>
2023-10-25 10:54:34 +0530

    test

---
+++ java/java.buildpacks/src/main/java/samples/choreo/io/GreetingController.java
@@ -0,0 +1,17 @@
+package samples.choreo.io;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+     @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name) {
+        GreetingService greetingService = new GreetingService();
+        String greeting = greetingService.greet(name);
+
+        return greeting;
+    }
+}
---
+++ java/java.buildpacks/src/main/java/samples/choreo/io/GreetingService.java
@@ -0,0 +1,8 @@
+package samples.choreo.io;
+
+public class GreetingService {
+
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
